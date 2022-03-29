e2b65e2106c95ec620f5a9e0238d359a51f719d7
Alan <ong80@hotmail.com>
2022-03-29 21:06:05 +0800

    E-truwise Front Page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
+  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center" style="background-color:#e7d4ee;">
     <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
+      <h1 class="text-5xl" style="color:black;" >Welcome to E-Truwise!</h1 >
+      <p class="mt-5" style="color:black;">Build, Manage and Maintain Your Customer.</p>
+      <img src="https://static.wixstatic.com/media/9c185e_2b6d12920c424f7b8eb9f7b596e8ab64~mv2.png/v1/fill/w_674,h_365,al_c,q_90/9c185e_2b6d12920c424f7b8eb9f7b596e8ab64~mv2.webp">
     </div>
   </div>
 </template>
