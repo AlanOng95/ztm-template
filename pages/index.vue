8f4e62f25cf6c7c04f365d882363e33536e37579
AlanOng95 <ong80@hotmail.com>
2022-03-30 20:10:37 +0800

    BoldText

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center" style="background-color:#e7d4ee;">
     <div class="m-auto">
-<<<<<<< HEAD
       <h1 class="text-5xl" style="color:black;font-weight: bold;" >Welcome to E-Truwise!</h1 >
-=======
-      <h1 class="text-5xl" style="color:black;font-weight: bold" >Welcome to E-Truwise!</h1 >
->>>>>>> ba6279d427deeb6e876d7e88b33d844c9ed7a11f
       <p class="mt-5" style="color:black;">Build, Manage and Maintain Your Customer.</p>
       <img src="https://static.wixstatic.com/media/9c185e_2b6d12920c424f7b8eb9f7b596e8ab64~mv2.png/v1/fill/w_674,h_365,al_c,q_90/9c185e_2b6d12920c424f7b8eb9f7b596e8ab64~mv2.webp">
     </div>
